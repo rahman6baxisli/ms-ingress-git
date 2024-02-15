6e772005454dec3777a0a929275e7e2ee7dcff77
rahman6baxisli <rahman.baxisli6@gmail.com>
2024-02-15 23:20:54 +0400

    new adde

--- src/az/ms/ingress/git/Main.java
+++ src/az/ms/ingress/git/Main.java
@@ -3,5 +3,6 @@ package az.ms.ingress.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World:");
+        System.out.println("Hello Rahman!");
     }
 }
